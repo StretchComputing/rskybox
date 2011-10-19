e85447f6221df76bc88b30087311740f0cdfd99e
Terry Roe <terryroe@gmail.com>
2011-10-19 14:53:18 -0400

    got user admin form to populate correctly

--- html/js/admin.js
+++ html/js/admin.js
@@ -1,29 +1,98 @@
-// set up the page(s) we need to build dynamically
-dynamicPages([{
-  page : 'item',
-  'function' : itemPage
-}]);
+// this is currently specific to /users, but that's all we have to admin right now
+$('#index').live('pageshow', function() {
+  var restUrl = REST_PREFIX + '/users';
 
-function showList(page, list) {
-  var content = page.find(':jqmData(role=content)');
+  jsonPopulate(restUrl, $('#index'), buildListPage);
+});
+
+function buildListPage(page, list) {
   var markup ='<ul data-role="listview">';
-  for (i = 0; i < list.length; i++) {
-    var user = list[i];
+  var users = list['users'];
+
+  for (i = 0; i < users.length; i++) {
+    var user = users[i];
     var display = user['firstName'] + ' ' + user['lastName'] + ': ' + user['emailAddress'];
     markup += '<li><a href="#item?id=' + user['id'] +'">' + display +'</a></li>';
   }
   markup += '</ul>'
-  content.html(markup);
-  content.find(':jqmData(role=listview)').listview();
+  pageContent(page, markup).find(':jqmData(role=listview)').listview();
 }
 
-$('#index').live('pageshow', function() {
-  $.mobile.showPageLoadingMsg();
-  $.getJSON(REST_PREFIX + '/users', function(list) {
-    showList($('#index'), list['users']);
-    $.mobile.hidePageLoadingMsg();
+
+// set up the page(s) we need to build dynamically
+dynamicPages([{
+  page: '#item',
+  'function': itemPage
+}]);
+
+$('#item').live('pagecreate', function() {
+  $(this).find('form').submit(saveItem);
+
+  $('#sendSmsNotifications').change(function() {
+    enableSmsDetails($('#sendSmsNotifications').prop('checked'));
   });
 });
+
+$('#delete').live('pagecreate', function() {
+  $('#delete_item').click(function() {
+    $.ajax({
+      type : 'DELETE',
+      url : REST_PREFIX + '/users/' + $('#id').val(),
+      contentType : 'application/json',
+      success : function(data) {
+        $.mobile.changePage('#index');
+      },
+      dataType : 'json'
+    });
+  });
+});
+
+function itemPage(page, url) {
+  var id = getParameterByName(url, 'id');
+  var restUrl = REST_PREFIX + '/mobileCarriers';
+
+  
+  page.find('form')[0].reset();
+  jsonPopulate(restUrl, $('#mobileCarrierId'), function(select, carriers) {
+    select.html(carrierOptions(carriers['mobileCarriers']));
+    if (id == 'new') {
+      $('#id').val('');
+      $('#sendEmailNotifications').prop('checked', false).checkboxradio('refresh');
+      $('#sendSmsNotifications').prop('checked', false).checkboxradio('refresh');
+    } else {
+      restUrl = REST_PREFIX + '/users/' + id;
+      jsonPopulate(restUrl, page, buildItemPage);
+    }
+  });
+}
+
+function buildItemPage(page, item) {
+  var smsEnabled = item['sendSmsNotifications'];
+
+  $('#id').val(item['id']);
+  $('#firstName').val(item['firstName']);
+  $('#lastName').val(item['lastName']);
+  $('#emailAddress').val(item['emailAddress']);
+
+  $('#sendEmailNotifications').prop('checked', item['sendEmailNotifications']).checkboxradio('refresh');
+  $('#sendSmsNotifications').prop('checked', smsEnabled).checkboxradio('refresh');
+  enableSmsDetails(smsEnabled);
+  $('#phoneNumber').val(item['phoneNumber']);
+  $('#mobileCarrierId').val(item['mobileCarrierId']);
+
+  page.page();
+  page.trigger('create');
+}
+
+function switchPage(url, options) {
+  var pageSelector = url.hash.replace(/\?.*$/, '');
+  var page = $(pageSelector);
+
+  enableSmsDetails($('#sendSmsNotifications').prop('checked'));
+  options.dataUrl = url.href;
+  $.mobile.changePage(page, options);
+  $.mobile.hidePageLoadingMsg();
+}
 
 //   function saveOtherItem() {
 //     var method, url;
@@ -72,88 +141,3 @@ function saveItem() {
   });
   return false;
 }
-
-
-function itemPage(url, options) {
-  $.mobile.showPageLoadingMsg();
-  $('#item').find('form')[0].reset();
-  var id = url.hash.replace(/.*id=/, '');
-  $.getJSON(REST_PREFIX + '/mobileCarriers', function(carriers) {
-    $('#mobileCarrierId').html(carrierOptions(carriers['mobileCarriers']));
-    if (id == 'new') {
-      switchPage(url, options);
-    } else {
-      $.getJSON(REST_PREFIX + '/users/' + id, function(item) {
-        setupItem(item, url, options);
-        switchPage(url, options);
-      });
-    }
-  });
-}
-
-function setupItem(item) {
-  $('#id').val(item['id']);
-  $('#firstName').val(item['firstName']);
-  $('#lastName').val(item['lastName']);
-  $('#emailAddress').val(item['emailAddress']);
-
-  $('#sendEmailNotifications').prop('checked', item['sendEmailNotifications']);
-  $('#sendSmsNotifications').prop('checked', item['sendSmsNotifications']);
-  $('#phoneNumber').val(item['phoneNumber']);
-  $('#mobileCarrierId').val(item['mobileCarrierId']);
-}
-
-function switchPage(url, options) {
-  var pageSelector = url.hash.replace(/\?.*$/, '');
-  var page = $(pageSelector);
-
-  enableSmsDetails($('#sendSmsNotifications').prop('checked'));
-  page.page();
-  page.trigger('create');
-
-  options.dataUrl = url.href;
-  $.mobile.changePage(page, options);
-  $.mobile.hidePageLoadingMsg();
-}
-
-function enableSmsDetails(enabled) {
-  $('#phoneNumber').prop('disabled', !enabled);
-  $('#mobileCarrierId').prop('disabled', !enabled);
-}
-
-function validateSms() {
-  if (!$('#sendSmsNotifications').prop('checked')) { return true; }
-
-  if (!validPhoneNumber($('#phoneNumber').val())) {
-    alert('Please enter a valid phone number.');
-    return false;
-  }
-  if ($('#mobileCarrierId').val() == NO_CARRIER) {
-    alert('Please select a mobile carrier.');
-    return false;
-  }
-
-  return true;
-}
-
-$('#item').live('pagecreate', function() {
-  console.log('pagecreate fired');
-  $(this).find('form').submit(saveItem);
-  $('#sendSmsNotifications').change(function() {
-    enableSmsDetails($('#sendSmsNotifications').prop('checked'));
-  });
-});
-
-$('#delete').live('pagecreate', function() {
-  $('#delete_item').click(function() {
-    $.ajax({
-      type : 'DELETE',
-      url : REST_PREFIX + '/users/' + $('#id').val(),
-      contentType : 'application/json',
-      success : function(data) {
-        $.mobile.changePage('#index');
-      },
-      dataType : 'json'
-    });
-  });
-});
