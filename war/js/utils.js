d323ae71d5716becbe773a98d2e8245d550db1e8
Terry Roe <terryroe@gmail.com>
2011-10-19 13:40:16 -0400

    refactored options functionality

--- war/js/utils.js
+++ war/js/utils.js
@@ -17,3 +17,24 @@ function carrierOptions(carriers) {
   }
   return markup;
 }
+
+
+function enableSmsDetails(enabled) {
+  $('#phoneNumber').prop('disabled', !enabled);
+  $('#mobileCarrierId').prop('disabled', !enabled);
+}
+
+function validateSms() {
+  if (!$('#sendSmsNotifications').prop('checked')) { return true; }
+
+  if (!validPhoneNumber($('#phoneNumber').val())) {
+    alert('Please enter a valid phone number.');
+    return false;
+  }
+  if ($('#mobileCarrierId').val() == NO_CARRIER) {
+    alert('Please select a mobile carrier.');
+    return false;
+  }
+
+  return true;
+}
