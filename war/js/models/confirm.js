66b9f405be5ce181f013ce45f3dba17a031729f3
Terry Roe <terryroe@gmail.com>
2012-04-10 09:53:19 -0400

    #109 - try/catch confirm model

--- war/js/models/confirm.js
+++ war/js/models/confirm.js
@@ -6,7 +6,11 @@ var RSKYBOX = (function (r, $) {
     apiUrl: '/users',
 
     initialize: function () {
-      this.setUrl();
+      try {
+        this.setUrl();
+      } catch (e) {
+        r.log.error(e, 'Confirm.initialize');
+      }
     },
 
     fields: {
@@ -19,51 +23,55 @@ var RSKYBOX = (function (r, $) {
     },
 
     validate: function (attrs) {
-      var
-        PASSWORD_MIN_LEN = 6,
-        CONFIRMATION_CODE_LEN = 3,
-        emailCode,
-        password,
-        phoneCode,
-        message = '';
+      try {
+        var
+          PASSWORD_MIN_LEN = 6,
+          CONFIRMATION_CODE_LEN = 3,
+          emailCode,
+          password,
+          phoneCode,
+          message = '';
 
-      if (attrs.emailAddress && attrs.emailAddress.length > 0) {
-        emailCode = attrs.emailConfirmationCode;
-        if (emailCode) {
-          if (emailCode.length === CONFIRMATION_CODE_LEN) {
-            r.log.info('valid email confirmation code', 'Confirm.validate');
-          } else {
-            message += 'Confirmation code must be exactly 3 characters. ';
+        if (attrs.emailAddress && attrs.emailAddress.length > 0) {
+          emailCode = attrs.emailConfirmationCode;
+          if (emailCode) {
+            if (emailCode.length === CONFIRMATION_CODE_LEN) {
+              r.log.info('valid email confirmation code', 'Confirm.validate');
+            } else {
+              message += 'Confirmation code must be exactly 3 characters. ';
+            }
           }
         }
-      }
 
-      // Testing for presence of confirmation code error message.
-      // So this test must follow the previous confirmation code test.
-      if (!message && attrs.phoneNumber && attrs.phoneNumber.length > 0) {
-        phoneCode = attrs.phoneConfirmationCode;
-        if (phoneCode) {
-          if (phoneCode.length === CONFIRMATION_CODE_LEN) {
-            r.log.info('valid phone confirmation code', 'Confirm.validate');
-          } else {
-            message += 'Confirmation code must be exactly 3 characters. ';
+        // Testing for presence of confirmation code error message.
+        // So this test must follow the previous confirmation code test.
+        if (!message && attrs.phoneNumber && attrs.phoneNumber.length > 0) {
+          phoneCode = attrs.phoneConfirmationCode;
+          if (phoneCode) {
+            if (phoneCode.length === CONFIRMATION_CODE_LEN) {
+              r.log.info('valid phone confirmation code', 'Confirm.validate');
+            } else {
+              message += 'Confirmation code must be exactly 3 characters. ';
+            }
           }
         }
-      }
 
-      if (!emailCode && !phoneCode) {
-        message += 'Confirmation code is required. ';
-      }
+        if (!emailCode && !phoneCode) {
+          message += 'Confirmation code is required. ';
+        }
 
-      password = attrs.password;
-      r.log.info(attrs.new, 'Confirm.validate');
-      if (!attrs.new || (password && password.length >= PASSWORD_MIN_LEN)) {
-        r.log.info('valid password', 'Confirm.validate');
-      } else {
-        message += 'Password must be at least 6 characters. ';
-      }
+        password = attrs.password;
+        r.log.info(attrs.new, 'Confirm.validate');
+        if (!attrs.new || (password && password.length >= PASSWORD_MIN_LEN)) {
+          r.log.info('valid password', 'Confirm.validate');
+        } else {
+          message += 'Password must be at least 6 characters. ';
+        }
 
-      if (message) { return message; }
+        if (message) { return message; }
+      } catch (e) {
+        r.log.error(e, 'Confirm.validate');
+      }
     }
   });
 
