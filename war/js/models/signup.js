1b538680e1b2da6912ba6e4df0e9cfd424f4c576
Terry Roe <terryroe@gmail.com>
2012-04-10 10:18:17 -0400

    #109 - try/catch signup model

--- war/js/models/signup.js
+++ war/js/models/signup.js
@@ -11,23 +11,35 @@ var RSKYBOX = (function (r, $) {
     },
 
     initialize: function () {
-      this.setUrl();
+      try {
+        this.setUrl();
+      } catch (e) {
+        r.log.error(e, 'Signup.initialize');
+      }
     },
 
     parse: function (response) {
-      r.log.info('model not needed after successful save', 'Signup.parse');
+      try {
+        r.log.info('model not needed after successful save', 'Signup.parse');
+      } catch (e) {
+        r.log.error(e, 'Signup.parse');
+      }
     },
 
     validate: function (attrs) {
-      if (r.isValidEmailAddress(attrs.emailAddress)) {
-        r.log.info('valid emailAddress', 'Signup.validate');
-        return;
+      try {
+        if (r.isValidEmailAddress(attrs.emailAddress)) {
+          r.log.info('valid emailAddress', 'Signup.validate');
+          return;
+        }
+        if (r.isValidPhoneNumber(attrs.phoneNumber) && attrs.mobileCarrierId) {
+          r.log.info('valid phone credentials', 'Signup.validate');
+          return;
+        }
+        return 'A valid email address -OR- valid phone number and mobile carrier is required.';
+      } catch (e) {
+        r.log.error(e, 'Signup.validate');
       }
-      if (r.isValidPhoneNumber(attrs.phoneNumber) && attrs.mobileCarrierId) {
-        r.log.info('valid phone credentials', 'Signup.validate');
-        return;
-      }
-      return 'A valid email address -OR- valid phone number and mobile carrier is required.';
     }
   });
 
