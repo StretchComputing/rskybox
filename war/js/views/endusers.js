49ec966e1b39f2cbc8d6a9554750f7e553485280
Terry Roe <terryroe@gmail.com>
2012-04-11 09:49:37 -0400

    #109 - try/catch enduser view

--- war/js/views/endusers.js
+++ war/js/views/endusers.js
@@ -6,74 +6,110 @@ var RSKYBOX = (function (r, $) {
     tagName: 'li',
 
     initialize: function () {
-      _.bindAll(this, 'render');
-      this.template = _.template($('#enduserEntryTemplate').html());
+      try {
+        _.bindAll(this, 'render');
+        this.template = _.template($('#enduserEntryTemplate').html());
+      } catch (e) {
+        r.log.error(e, 'EnduserEntryView.initialize');
+      }
     },
 
     render: function () {
-      this.$el.html(this.template(this.model.getMock()));
-      return this;
+      try {
+        this.$el.html(this.template(this.model.getMock()));
+        return this;
+      } catch (e) {
+        r.log.error(e, 'EnduserEntryView.render');
+      }
     }
   });
 
   r.EndusersView = r.JqmPageBaseView.extend({
     initialize: function () {
-      _.bindAll(this, 'addEnduserEntry');
-      this.collection.bind('reset', this.render, this);
-      this.template = _.template($('#noEndusersTemplate').html());
+      try {
+        _.bindAll(this, 'addEnduserEntry');
+        this.collection.bind('reset', this.render, this);
+        this.template = _.template($('#noEndusersTemplate').html());
+      } catch (e) {
+        r.log.error(e, 'EndusersView.initialize');
+      }
     },
 
     render: function () {
-      var list;
+      try {
+        var list;
 
-      this.getContent().empty();
-      if (this.collection.length <= 0) {
-        this.getContent().html(this.template());
-      } else {
-        list = $('<ul>');
-        this.collection.each(function (enduser) {
-          this.addEnduserEntry(list, enduser);
-        }, this);
-        this.getContent().html(list);
-        list.listview();
+        this.getContent().empty();
+        if (this.collection.length <= 0) {
+          this.getContent().html(this.template());
+        } else {
+          list = $('<ul>');
+          this.collection.each(function (enduser) {
+            this.addEnduserEntry(list, enduser);
+          }, this);
+          this.getContent().html(list);
+          list.listview();
+        }
+        return this;
+      } catch (e) {
+        r.log.error(e, 'EndusersView.render');
       }
-      return this;
     },
 
     addEnduserEntry: function (list, enduser) {
-      list.append(new r.EnduserEntryView({ model: enduser }).render().el);
+      try {
+        list.append(new r.EnduserEntryView({ model: enduser }).render().el);
+      } catch (e) {
+        r.log.error(e, 'EndusersView.addEnduserEntry');
+      }
     }
   });
 
 
   r.EnduserView = r.JqmPageBaseView.extend({
     initialize: function () {
-      this.model.on('change', this.render, this);
-      this.model.on('error', this.error, this);
-      this.template = _.template($('#enduserTemplate').html());
+      try {
+        this.model.on('change', this.render, this);
+        this.model.on('error', this.error, this);
+        this.template = _.template($('#enduserTemplate').html());
+      } catch (e) {
+        r.log.error(e, 'EnduserView.initialize');
+      }
     },
 
     render: function () {
-      this.getContent().html(this.template(this.model.getMock()));
-      this.getContent().trigger('create');
-      return this;
+      try {
+        this.getContent().html(this.template(this.model.getMock()));
+        this.getContent().trigger('create');
+        return this;
+      } catch (e) {
+        r.log.error(e, 'EnduserView.render');
+      }
     },
 
     error: function (model, response) {
-      if (response.responseText) { return; }  // This is an apiError.
-      r.log.info(response, 'EnduserView.error');
-      r.flash.warning(response);              // This is a validation error.
+      try {
+        if (response.responseText) { return; }  // This is an apiError.
+        r.log.info(response, 'EnduserView.error');
+        r.flash.warning(response);              // This is a validation error.
+      } catch (e) {
+        r.log.error(e, 'EnduserView.error');
+      }
     },
 
     //  This may not be in use.  Thanks Joe for pointing out my mistakes.
     apiError: function (jqXHR) {
-      var code = r.getApiStatus(jqXHR.responseText);
-      r.log.info(code, 'EnduserView.apiError');
+      try {
+        var code = r.getApiStatus(jqXHR.responseText);
+        r.log.info(code, 'EnduserView.apiError');
 
-      if (!this.apiCodes[code]) {
-        r.log.warn('Undefined apiStatus: ' + code, 'EnduserView.apiError');
+        if (!this.apiCodes[code]) {
+          r.log.warn('Undefined apiStatus: ' + code, 'EnduserView.apiError');
+        }
+        r.flash.warning(this.apiCodes[code]);
+      } catch (e) {
+        r.log.error(e, 'EnduserView.apiError');
       }
-      r.flash.warning(this.apiCodes[code]);
     },
 
     apiCodes: {
