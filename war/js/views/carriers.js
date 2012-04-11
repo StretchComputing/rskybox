deaa4fc5b3de575fef9dbe7ccd8e48c0f5a61de9
Terry Roe <terryroe@gmail.com>
2012-04-11 08:54:23 -0400

    #109 - try/catch carrier view

--- war/js/views/carriers.js
+++ war/js/views/carriers.js
@@ -6,33 +6,53 @@ var RSKYBOX = (function (r, $) {
     tagName: 'option',
 
     initialize: function () {
-      _.bindAll(this, 'render');
+      try {
+        _.bindAll(this, 'render');
+      } catch (e) {
+        r.log.error(e, 'CarrierView.initialize');
+      }
     },
 
     render: function () {
-      this.$el.attr('value', this.model.get('id')).html(this.model.get('name'));
-      return this;
+      try {
+        this.$el.attr('value', this.model.get('id')).html(this.model.get('name'));
+        return this;
+      } catch (e) {
+        r.log.error(e, 'CarrierView.render');
+      }
     }
   });
 
   r.CarriersView = Backbone.View.extend({
     initialize: function () {
-      _.bindAll(this, 'addCarrier');
-      this.collection.bind('reset', this.render, this);
-      this.value = '';
+      try {
+        _.bindAll(this, 'addCarrier');
+        this.collection.bind('reset', this.render, this);
+        this.value = '';
+      } catch (e) {
+        r.log.error(e, 'CarriersView.initialize');
+      }
     },
 
     render: function () {
-      this.$el.empty();
-      this.addCarrier(new r.Carrier({ id: '', name: 'Select Mobile Carrier'}));
-      this.collection.each(this.addCarrier);
-      this.$el.val(this.value);
-      this.$el.selectmenu('refresh');
-      return this;
+      try {
+        this.$el.empty();
+        this.addCarrier(new r.Carrier({ id: '', name: 'Select Mobile Carrier'}));
+        this.collection.each(this.addCarrier);
+        this.$el.val(this.value);
+        this.$el.selectmenu('refresh');
+        return this;
+      } catch (e) {
+        r.log.error(e, 'CarriersView.render');
+      }
     },
 
     addCarrier: function (carrier) {
-      this.$el.append(new r.CarrierView({ model: carrier }).render().el);
+      try {
+        this.$el.append(new r.CarrierView({ model: carrier }).render().el);
+      } catch (e) {
+        r.log.error(e, 'CarriersView.addCarrier');
+      }
     }
   });
 
