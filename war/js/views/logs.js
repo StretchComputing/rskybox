2071fafa83be53bd1e08e11584cf9c010066cdf8
Terry Roe <terryroe@gmail.com>
2012-04-11 11:32:40 -0400

    #109 - try/catch log view

--- war/js/views/logs.js
+++ war/js/views/logs.js
@@ -6,45 +6,65 @@ var RSKYBOX = (function (r, $) {
     tagName: 'li',
 
     initialize: function () {
-      _.bindAll(this, 'render');
-      this.template = _.template($('#logEntryTemplate').html());
+      try {
+        _.bindAll(this, 'render');
+        this.template = _.template($('#logEntryTemplate').html());
+      } catch (e) {
+        r.log.error(e, 'LogEntryView.initialize');
+      }
     },
 
     render: function () {
-      var mock = this.model.getMock();
+      try {
+        var mock = this.model.getMock();
 
-      mock.date = r.format.longDate(mock.date, true);
-      this.$el.html(this.template(mock));
-      return this;
+        mock.date = r.format.longDate(mock.date, true);
+        this.$el.html(this.template(mock));
+        return this;
+      } catch (e) {
+        r.log.error(e, 'LogEntryView.render');
+      }
     }
   });
 
   r.LogsView = r.JqmPageBaseView.extend({
     initialize: function () {
-      _.bindAll(this, 'addLogEntry');
-      this.collection.bind('reset', this.render, this);
-      this.template = _.template($('#noLogsTemplate').html());
+      try {
+        _.bindAll(this, 'addLogEntry');
+        this.collection.bind('reset', this.render, this);
+        this.template = _.template($('#noLogsTemplate').html());
+      } catch (e) {
+        r.log.error(e, 'LogsView.initialize');
+      }
     },
 
     render: function () {
-      var list;
+      try {
+        var list;
 
-      this.getContent().empty();
-      if (this.collection.length <= 0) {
-        this.getContent().html(this.template());
-      } else {
-        list = $('<ul>');
-        this.collection.each(function (log) {
-          this.addLogEntry(list, log);
-        }, this);
-        this.getContent().html(list);
-        list.listview();
+        this.getContent().empty();
+        if (this.collection.length <= 0) {
+          this.getContent().html(this.template());
+        } else {
+          list = $('<ul>');
+          this.collection.each(function (log) {
+            this.addLogEntry(list, log);
+          }, this);
+          this.getContent().html(list);
+          list.listview();
+        }
+        return this;
+      } catch (e) {
+        r.log.error(e, 'LogsView.render');
       }
-      return this;
     },
 
     addLogEntry: function (list, log) {
-      list.append(new r.LogEntryView({ model: log }).render().el);
+      try {
+        list.append(new r.LogEntryView({ model: log }).render().el);
+      } catch (e) {
+        r.log.error(e, 'LogsView.addLogEntry');
+      }
     }
   });
 
@@ -56,60 +76,84 @@ var RSKYBOX = (function (r, $) {
     },
 
     initialize: function () {
-      _.bindAll(this, 'changeStatus', 'changeMode', 'success');
-      this.model.on('change', this.render, this);
-      this.model.on('error', this.error, this);
-      this.template = _.template($('#logTemplate').html());
+      try {
+        _.bindAll(this, 'changeStatus', 'changeMode', 'success');
+        this.model.on('change', this.render, this);
+        this.model.on('error', this.error, this);
+        this.template = _.template($('#logTemplate').html());
+      } catch (e) {
+        r.log.error(e, 'LogView.');
+      }
     },
 
     render: function () {
-      var mock = this.model.getMock();
+      try {
+        var mock = this.model.getMock();
 
-      mock.date = r.format.longDate(mock.date, true);
-      if (mock.stackBackTrace && mock.stackBackTrace.length > 1) {
-        mock.stackBackTrace = mock.stackBackTrace.join('<br>');
+        mock.date = r.format.longDate(mock.date, true);
+        if (mock.stackBackTrace && mock.stackBackTrace.length > 1) {
+          mock.stackBackTrace = mock.stackBackTrace.join('<br>');
+        }
+        this.getContent().html(this.template(mock));
+        this.$el.trigger('create');
+        return this;
+      } catch (e) {
+        r.log.error(e, 'LogView.render');
       }
-      this.getContent().html(this.template(mock));
-      this.$el.trigger('create');
-      return this;
     },
 
-    changeMode: function (e) {
-      var json;
+    changeMode: function (evt) {
+      try {
+        var json;
 
-      json = JSON.stringify({
-        mode : (this.model.get('logMode') === 'inactive' ? 'active' : 'inactive')
-      });
-      $.ajax({
-        url: this.model.urlRoot + '/remoteControl/' + this.model.get('logName'),
-        type: 'PUT',
-        data: json,
-        success: this.success,
-        statusCode: r.statusCodeHandlers(this.apiError),
-      });
+        json = JSON.stringify({
+          mode : (this.model.get('logMode') === 'inactive' ? 'active' : 'inactive')
+        });
+        $.ajax({
+          url: this.model.urlRoot + '/remoteControl/' + this.model.get('logName'),
+          type: 'PUT',
+          data: json,
+          success: this.success,
+          statusCode: r.statusCodeHandlers(this.apiError),
+        });
 
-      e.preventDefault();
-      return false;
+        evt.preventDefault();
+        return false;
+      } catch (e) {
+        r.log.error(e, 'LogView.changeMode');
+      }
     },
 
     success: function () {
-      this.model.fetch();
+      try {
+        this.model.fetch();
+      } catch (e) {
+        r.log.error(e, 'LogView.succes');
+      }
     },
 
     error: function (model, response) {
-      if (response.responseText) { return; }  // This is an apiError.
-      r.log.info(response, 'LogView.error');
-      r.flash.warning(response);              // This is a validation error.
+      try {
+        if (response.responseText) { return; }  // This is an apiError.
+        r.log.info(response, 'LogView.error');
+        r.flash.warning(response);              // This is a validation error.
+      } catch (e) {
+        r.log.error(e, 'LogView.error');
+      }
     },
 
     apiError: function (jqXHR) {
-      var code = r.getApiStatus(jqXHR.responseText);
-      r.log.info(code, 'LogView.apiError');
+      try {
+        var code = r.getApiStatus(jqXHR.responseText);
+        r.log.info(code, 'LogView.apiError');
 
-      if (!this.apiCodes[code]) {
-        r.log.warn('Undefined apiStatus: ' + code, 'LogView.apiError');
+        if (!this.apiCodes[code]) {
+          r.log.warn('Undefined apiStatus: ' + code, 'LogView.apiError');
+        }
+        r.flash.warning(this.apiCodes[code]);
+      } catch (e) {
+        r.log.error(e, 'LogView.apiError');
       }
-      r.flash.warning(this.apiCodes[code]);
     },
 
     apiCodes: {
