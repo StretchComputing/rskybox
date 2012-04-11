2cb1e68b02c0a27329034278aa27c2121b459336
Terry Roe <terryroe@gmail.com>
2012-04-11 11:00:50 -0400

    #109 - try/catch feedback view

--- war/js/views/feedback.js
+++ war/js/views/feedback.js
@@ -6,45 +6,65 @@ var RSKYBOX = (function (r, $) {
     tagName: 'li',
 
     initialize: function () {
-      _.bindAll(this, 'render');
-      this.template = _.template($('#feedbackEntryTemplate').html());
+      try {
+        _.bindAll(this, 'render');
+        this.template = _.template($('#feedbackEntryTemplate').html());
+      } catch (e) {
+        r.log.error(e, 'FeedbackEntryView.initialize');
+      }
     },
 
     render: function () {
-      var mock = this.model.getMock();
+      try {
+        var mock = this.model.getMock();
 
-      mock.date = r.format.longDate(mock.date);
-      this.$el.html(this.template(mock));
-      return this;
+        mock.date = r.format.longDate(mock.date);
+        this.$el.html(this.template(mock));
+        return this;
+      } catch (e) {
+        r.log.error(e, 'FeedbackEntryView.render');
+      }
     }
   });
 
 
   r.FeedbackListView = r.JqmPageBaseView.extend({
     initialize: function () {
-      this.collection.bind('reset', this.render, this);
-      this.noFeedbackTemplate = _.template($('#noFeedbackTemplate').html());
+      try {
+        this.collection.bind('reset', this.render, this);
+        this.noFeedbackTemplate = _.template($('#noFeedbackTemplate').html());
+      } catch (e) {
+        r.log.error(e, 'FeedbackListView.initialize');
+      }
     },
 
     render: function () {
-      var list;
+      try {
+        var list;
 
-      this.getContent().empty();
-      if (this.collection.length <= 0) {
-        this.getContent().html(this.noFeedbackTemplate());
-      } else {
-        list = $('<ul>');
-        this.collection.each(function (feedback) {
-          this.addFeedbackEntry(list, feedback);
-        }, this);
-        this.getContent().html(list);
-        list.listview();
+        this.getContent().empty();
+        if (this.collection.length <= 0) {
+          this.getContent().html(this.noFeedbackTemplate());
+        } else {
+          list = $('<ul>');
+          this.collection.each(function (feedback) {
+            this.addFeedbackEntry(list, feedback);
+          }, this);
+          this.getContent().html(list);
+          list.listview();
+        }
+        return this;
+      } catch (e) {
+        r.log.error(e, 'FeedbackListView.render');
       }
-      return this;
     },
 
     addFeedbackEntry: function (list, feedback) {
-      list.append(new r.FeedbackEntryView({ model: feedback }).render().el);
+      try {
+        list.append(new r.FeedbackEntryView({ model: feedback }).render().el);
+      } catch (e) {
+        r.log.error(e, 'FeedbackListView.addFeedbackEntry');
+      }
     }
   });
 
@@ -56,36 +76,52 @@ var RSKYBOX = (function (r, $) {
     },
 
     initialize: function () {
-      _.bindAll(this, 'changeStatus');
-      this.model.on('change', this.render, this);
-      this.model.on('error', this.error, this);
-      this.template = _.template($('#feedbackTemplate').html());
+      try {
+        _.bindAll(this, 'changeStatus');
+        this.model.on('change', this.render, this);
+        this.model.on('error', this.error, this);
+        this.template = _.template($('#feedbackTemplate').html());
+      } catch (e) {
+        r.log.error(e, 'FeedbackView.initialize');
+      }
     },
 
     render: function () {
-      var mock = this.model.getMock();
+      try {
+        var mock = this.model.getMock();
 
-      mock.date = r.format.longDate(mock.date);
-      this.getContent().html(this.template(mock));
-      this.$el.trigger('create');
-      return this;
+        mock.date = r.format.longDate(mock.date);
+        this.getContent().html(this.template(mock));
+        this.$el.trigger('create');
+        return this;
+      } catch (e) {
+        r.log.error(e, 'FeedbackView.render');
+      }
     },
 
     error: function (model, response) {
-      if (response.responseText) { return; }  // This is an apiError.
-      r.log.info(response, 'FeedbackView.error');
-      r.flash.warning(response);              // This is a validation error.
+      try {
+        if (response.responseText) { return; }  // This is an apiError.
+        r.log.info(response, 'FeedbackView.error');
+        r.flash.warning(response);              // This is a validation error.
+      } catch (e) {
+        r.log.error(e, 'FeedbackView.error');
+      }
     },
 
     apiError: function (jqXHR) {
-      var code = r.getApiStatus(jqXHR.responseText);
-      r.log.info(code, 'FeedbackView.apiError');
+      try {
+        var code = r.getApiStatus(jqXHR.responseText);
+        r.log.info(code, 'FeedbackView.apiError');
 
-      if (!this.apiCodes[code]) {
-        r.log.warn('Undefined apiStatus: ' + code, 'FeedbackView.apiError');
+        if (!this.apiCodes[code]) {
+          r.log.warn('Undefined apiStatus: ' + code, 'FeedbackView.apiError');
+        }
+
+        r.flash.warning(this.apiCodes[code]);
+      } catch (e) {
+        r.log.error(e, 'FeedbackView.apiError');
       }
-
-      r.flash.warning(this.apiCodes[code]);
     },
 
     apiCodes: {
