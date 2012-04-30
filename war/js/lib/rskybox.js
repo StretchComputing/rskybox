d4badfd84ac663ccb41b32d240a6125d9f238b01
Terry Roe <terryroe@gmail.com>
2012-04-30 15:14:59 -0400

    #204 - fix logging level bugs, add auth items back into enduser create, reenable enduser create

--- war/js/lib/rskybox.js
+++ war/js/lib/rskybox.js
@@ -4,12 +4,12 @@ var RSKYBOX = (function (r, $) {
 
   var
     logLevels = {
+      off: 0,
       error: 5,
       warn: 10,
       info: 25,
       debug: 50,
       local: 75,
-      off: 99
     },
 
     apiCodes = {
@@ -210,6 +210,9 @@ var RSKYBOX = (function (r, $) {
         // Ensure attrs are valid for making an Ajax call.
         if (!isValid(attrs)) { return; }
 
+        delete attrs.appId;
+        delete attrs.authHeader;
+
         $.ajax({
           type: 'POST',
           data: JSON.stringify(attrs),
@@ -230,11 +233,11 @@ var RSKYBOX = (function (r, $) {
     // Traffic cop for determining where logs should go.
     base = function (level, message, name) {
       try {
-        if (r.config.getApplicationId() && (r.config.log.getServerLevel() >= logLevels[level])) {
+        if (r.config.getApplicationId() && (logLevels[level] <= r.config.log.getServerLevel())) {
           server(level, message, name);
         }
 
-        if (r.config.log.getLocalLevel() >= logLevels[level]) {
+        if (logLevels[level] <= r.config.log.getLocalLevel()) {
           local(r.config.getConsole(), level, message, name);
         }
       } catch (e) {
@@ -332,6 +335,8 @@ var RSKYBOX = (function (r, $) {
       try {
         var
           attrs = {
+            appId: r.config.getApplicationId(),
+            authHeader: r.config.getAuthHeader(),
             userId: r.config.getUserId(),
             userName: r.config.getUserName(),
             application: r.config.getApplicationName(),
@@ -344,6 +349,9 @@ var RSKYBOX = (function (r, $) {
 
         // Ensure attrs are valid for making an Ajax call.
         if (!isValid(attrs)) { return; }
+
+        delete attrs.appId;
+        delete attrs.authHeader;
 
         $.ajax({
           type: 'POST',
@@ -369,13 +377,13 @@ var RSKYBOX = (function (r, $) {
     },
   };
 
-  //$(function () {
-    //function sendToServer() {
-      //server();
-      //setTimeout(sendToServer, 15 * 60 * 1000); // every fifteen minutes
-    //}
-    //sendToServer();
-  //});
+  $(function () {
+    function sendToServer() {
+      server();
+      setTimeout(sendToServer, 15 * 60 * 1000); // every fifteen minutes
+    }
+    sendToServer();
+  });
 
 
   return r;
