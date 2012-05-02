45c3c55867f6762e18deddc46293d253d3d3d7ac
Terry Roe <terryroe@gmail.com>
2012-05-02 13:57:08 -0400

    #131 - work in progress

---
+++ war/js/models/iLog.js
@@ -0,0 +1,40 @@
+var RSKYBOX = (function (r, $) {
+  'use strict';
+
+
+  r.iLog = r.BaseModel.extend({
+    apiUrl: '/incidents',
+    fields: {
+      id: null,
+      appId: null,
+      number: null,
+      status: null,
+      severity: null,
+      name: null,
+      lastUpdatedDate: null,
+      createdDate: null,
+      tags: null,
+      eventCount: null,
+      summary: null,
+      message: null,
+      appActions: null,
+    },
+  });
+
+
+  r.iLogs = r.BaseCollection.extend({
+    model: r.iLog,
+    apiUrl: '/incidents?tag=log',
+
+    parse: function (response) {
+      try {
+        return response.incidents;
+      } catch (e) {
+        r.log.error(e, 'iLogs.parse');
+      }
+    }
+  });
+
+
+  return r;
+}(RSKYBOX || {}, jQuery));
---
+++ war/js/views/iLogs.js
@@ -0,0 +1,177 @@
+var RSKYBOX = (function (r, $) {
+  'use strict';
+
+
+  r.iLogEntryView = Backbone.View.extend({
+    tagName: 'li',
+
+    initialize: function () {
+      try {
+        _.bindAll(this, 'render');
+        this.template = _.template($('#iLogEntryTemplate').html());
+      } catch (e) {
+        r.log.error(e, 'iLogEntryView.initialize');
+      }
+    },
+
+    render: function () {
+      try {
+        var mock = this.model.getMock();
+
+        mock.lastUpdatedDate = r.format.longDate(mock.lastUpdatedDate);
+        this.$el.html(this.template(mock));
+        return this;
+      } catch (e) {
+        r.log.error(e, 'iLogEntryView.render');
+      }
+    }
+  });
+
+
+  r.iLogsView = r.JqmPageBaseView.extend({
+    initialize: function () {
+      try {
+        r.log.info('entering', 'iLogsView.initialize');
+        _.bindAll(this, 'addLogEntry');
+        this.collection.bind('reset', this.render, this);
+        this.template = _.template($('#iNoLogsTemplate').html());
+      } catch (e) {
+        r.log.error(e, 'iLogsView.initialize');
+      }
+    },
+
+    render: function () {
+      try {
+        var list;
+
+        this.appLink('back', 'application');
+
+        this.getContent().empty();
+        if (this.collection.length <= 0) {
+          this.getContent().html(this.template());
+        } else {
+          list = $('<ul>');
+          this.collection.each(function (log) {
+            this.addLogEntry(list, log);
+          }, this);
+          this.getContent().html(list);
+          list.listview();
+        }
+        return this;
+      } catch (e) {
+        r.log.error(e, 'iLogsView.render');
+      }
+    },
+
+    addLogEntry: function (list, log) {
+      try {
+        list.append(new r.iLogEntryView({ model: log }).render().el);
+      } catch (e) {
+        r.log.error(e, 'iLogsView.addLogEntry');
+      }
+    }
+  });
+
+
+  r.iLogView = r.JqmPageBaseView.extend({
+    events: {
+      'click .changeStatus': 'changeStatus',
+      'click .mode': 'changeMode',
+    },
+
+    initialize: function () {
+      try {
+        _.bindAll(this, 'changeStatus', 'changeMode', 'success');
+        this.model.on('change', this.render, this);
+        this.model.on('error', this.error, this);
+        this.template = _.template($('#logTemplate').html());
+      } catch (e) {
+        r.log.error(e, 'iLogView.');
+      }
+    },
+
+    render: function () {
+      try {
+        var mock = this.model.getMock();
+
+        this.appLink('back', 'ilogs');
+
+        mock.date = r.format.longDate(mock.date, true);
+        if (Array.isArray(mock.stackBackTrace)) {
+          mock.stackBackTrace = mock.stackBackTrace.join('<br>');
+        }
+        this.getContent().html(this.template(mock));
+        this.$el.trigger('create');
+        return this;
+      } catch (e) {
+        r.log.error(e, 'iLogView.render');
+      }
+    },
+
+    changeMode: function (evt) {
+      try {
+        var json;
+
+        json = JSON.stringify({
+          mode : (this.model.get('logMode') === 'inactive' ? 'active' : 'inactive')
+        });
+        $.ajax({
+          url: this.model.urlRoot + '/remoteControl/' + this.model.get('logName'),
+          type: 'PUT',
+          data: json,
+          success: this.success,
+          statusCode: r.statusCodeHandlers(this.apiError),
+        });
+
+        evt.preventDefault();
+        return false;
+      } catch (e) {
+        r.log.error(e, 'iLogView.changeMode');
+      }
+    },
+
+    success: function () {
+      try {
+        this.model.fetch();
+      } catch (e) {
+        r.log.error(e, 'iLogView.succes');
+      }
+    },
+
+    error: function (model, response) {
+      try {
+        if (response.responseText) { return; }  // This is an apiError.
+        r.log.info(response, 'iLogView.error');
+        r.flash.warning(response);              // This is a validation error.
+      } catch (e) {
+        r.log.error(e, 'iLogView.error');
+      }
+    },
+
+    apiError: function (jqXHR) {
+      try {
+        var code = r.getApiStatus(jqXHR.responseText);
+        r.log.info(code, 'iLogView.apiError');
+
+        if (!this.apiCodes[code]) {
+          r.log.warn('Undefined apiStatus: ' + code, 'iLogView.apiError');
+        }
+        r.flash.warning(this.apiCodes[code]);
+      } catch (e) {
+        r.log.error(e, 'iLogView.apiError');
+      }
+    },
+
+    apiCodes: {
+      201: 'Invalid status.',
+      203: 'You are not authorized for this application.',
+      302: 'Log ID required.',
+      305: 'Application ID required.',
+      603: 'Log was not found',
+      605: 'Application was not found',
+    }
+  });
+
+
+  return r;
+}(RSKYBOX || {}, jQuery));
